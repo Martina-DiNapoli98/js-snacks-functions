109bbcaf31482431f3b20f31ae7c5efdbabb0abf
Martina-dinapoli98 <martinadinapoli1998@gmail.com>
2025-02-17 15:53:42 +0100

    exercise 2

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,9 +4,26 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+/* function secretName(names){
+    let anotherName = []
+    for(let i = 0; i < names.length; i++){
+        thisName = names[i]
+        anotherName.push(thisName[0]);
+    }
+    return anotherName;
+} */
 
-
+let secretName = (names) => {
+    let anotherName = []
+    for(let i = 0; i < names.length; i++){
+        thisName = names[i]
+        anotherName.push(thisName[0]);
+    }
+    return anotherName;
+}
 // Invoca la funzione qui e stampa il risultato in console
+let newName = secretName(names);
+console.log(newName);
 
 
 
