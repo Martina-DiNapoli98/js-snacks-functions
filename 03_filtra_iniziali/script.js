4d1a354dd09f9645a6d12a11d17ce75ce1195b1b
Martina-dinapoli98 <martinadinapoli1998@gmail.com>
2025-02-17 18:24:19 +0100

    exercise 3

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -1,7 +1,23 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
+const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
+const firstLetter = "A";
+
+// Dichiara la funzione qui.
+function thisLetter(names, firstLetter){
+    let anotherArray = []
+    for(let i = 0; i < names.length; i++){
+        anotherNames = names[i]
+        if(firstLetter === anotherNames[0]){
+            anotherArray.push(anotherNames);
+        }
+    } 
+    return anotherArray;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+const firstLetterA = thisLetter(names, firstLetter);
+console.log(firstLetterA);
 
 
 
