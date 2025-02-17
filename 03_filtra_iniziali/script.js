079f8f97034e775ce454a632081ffa4673a770b0
Martina-dinapoli98 <martinadinapoli1998@gmail.com>
2025-02-17 18:26:10 +0100

    fix arrow function 3

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,7 +4,18 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 const firstLetter = "A";
 
 // Dichiara la funzione qui.
-function thisLetter(names, firstLetter){
+/* function thisLetter(names, firstLetter){
+    let anotherArray = []
+    for(let i = 0; i < names.length; i++){
+        anotherNames = names[i]
+        if(firstLetter === anotherNames[0]){
+            anotherArray.push(anotherNames);
+        }
+    } 
+    return anotherArray;
+} */
+
+const thisLetter = (names, firstLetter) => {
     let anotherArray = []
     for(let i = 0; i < names.length; i++){
         anotherNames = names[i]
@@ -18,6 +29,8 @@ function thisLetter(names, firstLetter){
 // Invoca la funzione qui e stampa il risultato in console
 const firstLetterA = thisLetter(names, firstLetter);
 console.log(firstLetterA);
+
+
 
 
 
